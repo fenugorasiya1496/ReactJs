91f3fb022d505f2192fb25e6208b72e27201e016
fenugorasiya1496 <fenugorasiya1496@gmail.com>
2026-03-16 19:42:31 +0530

    create index.html

---
+++ two-binding/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import LogIn from './Component/LogIn'
+
+
+const App = () => {
+  return (
+    <>
+      {/* Two way binding : manage form filed using usestate */}
+      <LogIn />
+    </>
+  )
+}
+
+
+export default App
+
+
+
